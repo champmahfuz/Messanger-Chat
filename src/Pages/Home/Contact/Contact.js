cdb7dc0a6c35abae38cdc78ff0c20e357cc07ee2
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2022-06-30 18:43:17 +0600

    added messanger

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -6,10 +6,8 @@ const Contact = () => {
             <h2>This is Contact</h2>
             <div>
                 <MessengerCustomerChat
-                    pageId="106289902139677"
-                    // 101722142602573
+                    pageId="101722142602573"
                     appId=" 71020473359128"
-                //app id:  71020473359128
                 />,
             </div>
         </div>
