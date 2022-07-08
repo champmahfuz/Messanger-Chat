f77bdc7343d5af77e3817093268389774e651b81
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2022-07-08 22:42:22 +0600

    added messanger

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -6,11 +6,11 @@ const Contact = () => {
             <h2>This is Contact</h2>
             <div>
                 <MessengerCustomerChat
-                    // pageId="101722142602573"
-                    // appId=" 71020473359128"
+                    pageId="101722142602573"
+                    appId=" 71020473359128"
 
-                    pageId="process.env.REACT_APP_PAGE_ID"
-                    appId="process.env.REACT_APP_APP_ID"
+                // pageId="process.env.REACT_APP_PAGE_ID"
+                // appId="process.env.REACT_APP_APP_ID"
                 />
             </div>
         </div>
